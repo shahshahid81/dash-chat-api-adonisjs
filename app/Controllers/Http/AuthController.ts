06a788a71007cd33e5c08acac228b326096e2f1c
Shahid Shah <shahshahid2123@gmail.com>
2023-03-04 18:04:46 +0530

    feat: added login and logout route

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,10 +1,21 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import AuthService from 'App/Services/AuthService'
+import LoginUserValidator from 'App/Validators/LoginUserValidator'
 import RegisterUserValidator from 'App/Validators/RegisterUserValidator'
 
 export default class AuthController {
   public async register({ request, auth }: HttpContextContract) {
     const payload = await request.validate(RegisterUserValidator)
-    return AuthService.registerUser(payload,auth)
+    return AuthService.registerUser(payload, auth)
+  }
+
+  public async login({ request, auth }: HttpContextContract) {
+    const payload = await request.validate(LoginUserValidator)
+    return auth.attempt(payload.email, payload.password)
+  }
+
+  public async logout({ auth }: HttpContextContract) {
+    await auth.logout()
+    return { success: true }
   }
 }
--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -3,4 +3,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
     Route.post('register', 'AuthController.register').as('register')
+    Route.post('login', 'AuthController.login').as('login')
+    Route.post('logout', 'AuthController.logout').as('logout').middleware('auth')
 }).prefix('api/auth').as('api-auth')
