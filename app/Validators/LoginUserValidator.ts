6af454379eb52ca8ab1dec4edb8c96e88eea6bdf
Shahid Shah <shahshahid2123@gmail.com>
2023-03-23 14:38:06 +0530

    feat: updated validation messages

--- app/Validators/LoginUserValidator.ts
+++ app/Validators/LoginUserValidator.ts
@@ -1,4 +1,4 @@
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class LoginUserValidator {
@@ -8,4 +8,13 @@ export default class LoginUserValidator {
     email: schema.string({ trim: true }, [rules.email()]),
     password: schema.string({ trim: true }, [rules.minLength(8), rules.maxLength(20)]),
   })
+
+  public messages: CustomMessages = {
+    'email.string': 'Enter a valid email',
+    'email.email': 'Enter a valid email',
+
+    'password.string': 'Enter a valid password',
+    'password.minLength': 'Password should be minimum {{ options.minLength }} characters',
+    'password.maxLength': 'Password should be maxLength {{ options.maxLength }} characters',
+  }
 }
--- app/Validators/RegisterUserValidator.ts
+++ app/Validators/RegisterUserValidator.ts
@@ -1,4 +1,4 @@
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { DateTime } from 'luxon'
 
@@ -32,4 +32,32 @@ export default class RegisterUserValidator {
       rules.after(this.refs.maximumAllowedDate),
     ]),
   })
+
+  public messages: CustomMessages = {
+    'email.string': 'Enter a valid email',
+    'email.email': 'Enter a valid email',
+
+    'password.string': 'Enter a valid password',
+    'password.minLength': 'Password should be minimum {{ options.minLength }} characters',
+    'password.maxLength': 'Password should be maxLength {{ options.maxLength }} characters',
+    'password.confirmPassword': 'Password and Confirm Password should be same',
+
+    'firstName.string': 'Enter a valid first name',
+    'firstName.alpha': 'First name should only contain alphabets',
+    'firstName.minLength': 'First name should be minimum {{ options.minLength }} characters',
+    'firstName.maxLength': 'First name should be maxLength {{ options.maxLength }} characters',
+
+    'lastName.string': 'Enter a valid last name',
+    'lastName.alpha': 'Last name should only contain alphabets',
+    'lastName.minLength': 'Last name should be minimum {{ options.minLength }} characters',
+    'lastName.maxLength': 'Last name should be maxLength {{ options.maxLength }} characters',
+
+    'dateOfBirth.date': 'Enter a valid date of birth in {{ options.format }} format',
+    'dateOfBirth.before': `Date of birth should be before ${this.refs.minimumAllowedDate.value.toFormat(
+      'yyyy-MM-dd'
+    )}`,
+    'dateOfBirth.after': `Date of birth should be after ${this.refs.maximumAllowedDate.value.toFormat(
+      'yyyy-MM-dd'
+    )}`,
+  }
 }
